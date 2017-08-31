9d3e318ef5c427cec9dbe5b6fcfb2290aa3ae8a0
Monzonprog <j.monzonm@gmail.com>
2017-08-31 19:11:44 +0200

    Ultimos cambios

---
+++ app/src/main/java/com/example/jorge/gasolinator/Interfaces/OpcionesTarjetaVehiculos.java
@@ -0,0 +1,11 @@
+package com.example.jorge.gasolinator.Interfaces;
+
+/**
+ * Created by jorge on 29/08/17.
+ */
+
+public interface OpcionesTarjetaVehiculos {
+
+    void modificarVehiculo(String id);
+    void eliminarVehiculo(String id);
+}
